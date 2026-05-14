6ab89206f5459a232358c4dbc0d2359fa6456585
Zeronetsec <senxhunter996@gmail.com>
2026-05-14 10:40:58 +0700

    modified: utils/help.sh

--- utils/help.sh
+++ utils/help.sh
@@ -12,15 +12,15 @@ function helper() {
     echo -e "${N}Usage: ${GG}chprompt ${CC}<command> [<args>]${N}"
     printf '\n'
     echo -e "${N}Available commands:"
-    echo -e "    ${GG}--preview ${DG}<${CC}line/prompt_number${DG}> ${N}preview a specific prompt"
-    echo -e "    ${GG}--use ${DG}<${CC}line/prompt_number${DG}> ${N}set a specific prompt as active"
-    echo -e "    ${GG}--inject ${DG}<${CC}line/prompt_number${DG}> ${N}inject a prompt into the ${CC}~/.bashrc ${N}configuration"
-    echo -e "    ${GG}--chuser ${DG}<${CC}user${DG}> [<${CC}new_user${DG}>] ${N}replace username in all plugins"
-    echo -e "    ${GG}--chhost ${DG}<${CC}host${DG}> [<${CC}new_host${DG}>] ${N}replace hostname in all plugins"
-    echo -e "    ${GG}--reset ${N}restore plugins to default (removes custom plugins)"
-    echo -e "    ${GG}--list ${N}list all available prompt numbers"
-    echo -e "    ${GG}--version ${N}show version information"
-    echo -e "    ${GG}--help ${N}show this help message"
+    echo -e "    ${DG}* ${GG}--preview ${DG}<${CC}line/prompt_number${DG}> ${N}preview a specific prompt"
+    echo -e "    ${DG}* ${GG}--use ${DG}<${CC}line/prompt_number${DG}> ${N}set a specific prompt as active"
+    echo -e "    ${DG}* ${GG}--inject ${DG}<${CC}line/prompt_number${DG}> ${N}inject a prompt into the ${CC}~/.bashrc ${N}configuration"
+    echo -e "    ${DG}* ${GG}--chuser ${DG}<${CC}user${DG}> [<${CC}new_user${DG}>] ${N}replace username in all plugins"
+    echo -e "    ${DG}* ${GG}--chhost ${DG}<${CC}host${DG}> [<${CC}new_host${DG}>] ${N}replace hostname in all plugins"
+    echo -e "    ${DG}* ${GG}--reset ${N}restore plugins to default (removes custom plugins)"
+    echo -e "    ${DG}* ${GG}--list ${N}list all available prompt numbers"
+    echo -e "    ${DG}* ${GG}--version ${N}show version information"
+    echo -e "    ${DG}* ${GG}--help ${N}show this help message"
 
     return $?
 }
