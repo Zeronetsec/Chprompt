4993b625d6ce7e4cb95b2f4a8df67e5820133e64
Zeronetsec <senxhunter996@gmail.com>
2026-07-23 04:19:24 +0700

    modified: bin/chprompt.sh

--- bin/chprompt.sh
+++ bin/chprompt.sh
@@ -23,11 +23,15 @@ export root="$(cd "${dir}/.." && pwd)"
 if [[ "${1}" == '--init' ]]; then
     bashrc="${2:-${HOME}/.bashrc}"
 
+    if [[ ! -f "${bashrc}" ]]; then
+        command touch "${bashrc}"
+    fi
+
     echo -ne "source ${root}/chprompt.sh" \
         >> "${bashrc}"
     ex=${?}
 
-    echo -e "${B}[*] ${N}Injected code: ${GG}source ${root}/chprompt.sh ${DG}-> ${GG}${bashrc}${N}"
+    echo -e "\x1b[1;34m[*] \x1b[0mInjected code: \x1b[0;32msource ${root}/chprompt.sh \x1b[1;90m-> \x1b[0;32m${bashrc}\x1b[0m"
     exit ${ex}
 fi
 
