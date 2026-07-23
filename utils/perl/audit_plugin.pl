85e420adcfc996073a5e21b093fe9a2c3f56d38e
Zeronetsec <senxhunter996@gmail.com>
2026-07-23 23:06:15 +0700

    modified: utils/perl/audit_plugin.pl

--- utils/perl/audit_plugin.pl
+++ utils/perl/audit_plugin.pl
@@ -16,31 +16,43 @@ my $CC = "\x1b[0;36m";
 sub load_patterns {
     my ($pattern_file_path) = @_;
 
-    if (!$pattern_file_path || ! -e $pattern_file_path) {
+    if (
+        !$pattern_file_path ||
+        ! -e $pattern_file_path
+    ) {
         my $path_display = $pattern_file_path // "";
         print "${R}[!] ${N}Pattern file: ${GG}${path_display} ${N}not found!\n";
         exit(1);
     }
 
     my @compiled_patterns;
-    open(my $fh, '<:encoding(UTF-8)', $pattern_file_path)
-        or die "${R}[!] ${N}Cannot open pattern file: $!\n";
+    open(
+        my $fh,'<:encoding(UTF-8)', $pattern_file_path,
+    ) or die "${R}[!] ${N}Cannot open pattern file: $!\n";
 
     while (my $line = <$fh>) {
         $line =~ s/^\s+|\s+$//g;
-        next if ($line eq '' || $line =~ /^#/);
+        next if (
+            $line eq '' ||
+            $line =~ /^#/
+        );
 
         my $escaped = quotemeta($line);
         my $first_char = substr($line, 0, 1);
         my $last_char = substr($line, -1);
 
-        my $prefix = ($first_char =~ /^[a-zA-Z0-9]$/) ? '\b' : '';
-        my $suffix = ($last_char =~ /^[a-zA-Z0-9]$/) ? '\b' : '';
+        my $prefix = (
+            $first_char =~ /^[a-zA-Z0-9]$/,
+        ) ? '\b' : '';
+
+        my $suffix = (
+            $last_char =~ /^[a-zA-Z0-9]$/,
+        ) ? '\b' : '';
 
         my $pattern_regex = "${prefix}${escaped}${suffix}";
-
         push @compiled_patterns, qr/$pattern_regex/;
     }
+
     close($fh);
     return \@compiled_patterns;
 }
@@ -49,21 +61,21 @@ sub scan_plugin {
     my ($plugin_dir, $patterns_ref) = @_;
     my @patterns = @$patterns_ref;
 
-    if (!$plugin_dir || ! -d $plugin_dir) {
+    if (
+        !$plugin_dir ||
+        ! -d $plugin_dir
+    ) {
         my $dir_display = $plugin_dir // "";
         print "${R}[!] ${N}Plugin directory: ${GG}${dir_display} ${N}not found!\n";
         exit(1);
     }
 
     my $match_count = 0;
-
     find(
         {
             wanted => sub {
                 return unless -f $_;
-
                 my $file_path = $File::Find::name;
-
                 eval {
                     open(
                         my $fh, '<:encoding(UTF-8)', $_
@@ -100,7 +112,10 @@ sub scan_plugin {
 my $PLUGIN_PATH  = $ENV{'plugin'};
 my $PATTERN_PATH = $ENV{'pattern'};
 
-if (!$PLUGIN_PATH || !$PATTERN_PATH) {
+if (
+    !$PLUGIN_PATH ||
+    !$PATTERN_PATH
+) {
     print "${R}[!] ${N}Missing variable: ${GG}plugin ${N}and ${GG}pattern${N}\n";
     exit(1);
 }
