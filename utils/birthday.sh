08f66c4371667ba827d342f562778a4aa82b9701
Zeronetsec <senxhunter996@gmail.com>
2026-07-27 20:34:25 +0700

    modified: utils/birthday.sh

--- utils/birthday.sh
+++ utils/birthday.sh
@@ -4,7 +4,7 @@ function utils::birthday() {
     local birth_date="03-10"
     local today="$(command date '+%m-%d')"
     if [[ "${today}" == "${birth_date}" ]]; then
-        echo -e "${R}› ${N}Happy birthday for ${GG}chprompt ${N}🎉"
+        echo -e "${R}› ${N}Happy birthday for ${GG}Chprompt ${N}🎉"
         printf '\n'
     fi
 }
